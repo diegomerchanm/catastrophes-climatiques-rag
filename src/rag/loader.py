e815d975a71acf21198e3198c5ccddd146323d54
Diego <diegomerchanm>
2026-04-08 11:59:26 +0200

    feat: fix import RecursiveCharacterTextSplitter - langchain_text_splitters

--- src/rag/loader.py
+++ src/rag/loader.py
@@ -2,7 +2,7 @@
 
 import os
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 def charger_documents(dossier: str) -> list:
